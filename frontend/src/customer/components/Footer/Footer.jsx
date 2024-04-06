a396575e34967dab38fb2eb29f584a02c351d114
Sherry <e24089020@gs.ncku.edu.tw>
2024-04-06 17:50:05 +0800

    adjust footer

--- frontend/src/customer/components/Footer/Footer.jsx
+++ frontend/src/customer/components/Footer/Footer.jsx
@@ -7,12 +7,13 @@ const Footer = () => {
     <div>
       <Grid
         container
-        className="bg-black text-white  mt-12"
+        className="bg-black text-white mt-12 "
         sx={{ bgcolor: "black", color: "white", py: 5 }}>
         <Grid
           item
           container
           sx={{
+            marginLeft: "40px",
             flex: 1,
             alignItems: "flex-start",
             display: "flex",
@@ -158,38 +159,43 @@ const Footer = () => {
           </Button>
         </Grid>
 
-        <Grid
-          item
-          className="pt-5"
-          xs={12}
-          container
-          alignItems="center"
-          justifyContent="space-around"
-          sx={{ borderTop: "1px solid gray" }}>
-          <Typography color="gray" sx={{ fontSize: "12px" }}>
-            2024 &copy; Eminent, Inc. (a Revolve Group company). All Rights
-            Reserved.
-          </Typography>
-          <div>
-            <Button sx={{ fontSize: "10px", color: "white" }}>TERMS</Button>
-            <Button sx={{ fontSize: "10px", color: "white" }}> PRIVACY</Button>
-            <Button sx={{ fontSize: "10px", color: "white" }}>
-              COOKIE PREFERENCES
-            </Button>
-            <Button sx={{ fontSize: "10px", color: "white" }}>
-              {" "}
-              CA PRIVACY RIGHTS
-            </Button>
-            <Button sx={{ fontSize: "10px", color: "white" }}>
-              {" "}
-              CA TRANSPARENCY ACT
-            </Button>
-            <Button sx={{ fontSize: "10px", color: "white" }}>
-              {" "}
-              YOUR PRIVACY CHOICES
-            </Button>
-          </div>
-        </Grid>
+        <div className="w-full  mt-10">
+          <Grid
+            item
+            className="pt-5"
+            xs={12}
+            container
+            alignItems="center"
+            justifyContent="space-around"
+            sx={{ borderTop: "1px solid gray" }}>
+            <Typography color="gray" sx={{ fontSize: "12px" }}>
+              2024 &copy; Eminent, Inc. (a Revolve Group company). All Rights
+              Reserved.
+            </Typography>
+            <div>
+              <Button sx={{ fontSize: "10px", color: "white" }}>TERMS</Button>
+              <Button sx={{ fontSize: "10px", color: "white" }}>
+                {" "}
+                PRIVACY
+              </Button>
+              <Button sx={{ fontSize: "10px", color: "white" }}>
+                COOKIE PREFERENCES
+              </Button>
+              <Button sx={{ fontSize: "10px", color: "white" }}>
+                {" "}
+                CA PRIVACY RIGHTS
+              </Button>
+              <Button sx={{ fontSize: "10px", color: "white" }}>
+                {" "}
+                CA TRANSPARENCY ACT
+              </Button>
+              <Button sx={{ fontSize: "10px", color: "white" }}>
+                {" "}
+                YOUR PRIVACY CHOICES
+              </Button>
+            </div>
+          </Grid>
+        </div>
       </Grid>
     </div>
   );
